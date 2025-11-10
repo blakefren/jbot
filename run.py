a86f5b1d23c9d3444fbee59f34e20e6528177f65
Blake French <blakefren@gmail.com>
2025-11-09 16:19:41 -0800

    Format run.py

--- run.py
+++ run.py
@@ -3,9 +3,9 @@ import os
 import runpy
 
 # Add the 'src' directory to the Python path
-src_dir = os.path.join(os.path.dirname(__file__), 'src')
+src_dir = os.path.join(os.path.dirname(__file__), "src")
 sys.path.insert(0, src_dir)
 
 if __name__ == "__main__":
     # Run the main module from the src directory
-    runpy.run_module('main', run_name='__main__')
+    runpy.run_module("main", run_name="__main__")
