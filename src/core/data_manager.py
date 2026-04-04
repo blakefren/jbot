3205d7141d3e3853adf81f55ba558c2aed2635a8
Blake French <blakefren@gmail.com>
2026-04-04 16:03:40 -0700

    Update data_manager.py

--- src/core/data_manager.py
+++ src/core/data_manager.py
@@ -11,7 +11,7 @@ import pytz
 # Project root for file paths
 _PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
 
-
+# test
 # TODO: review for SQL injection risks.
 class DataManager:
     """
